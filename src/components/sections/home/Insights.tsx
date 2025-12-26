01f38c180d609e31f9bdefdaa6e0c2e8ea0d0930
Devoq-Tushar <tushar@devoqtech.com>
2025-12-26 20:29:38 +0530

    Add files via upload
    Fix: add popup on Insights description.

--- src/components/sections/home/Insights.tsx
+++ src/components/sections/home/Insights.tsx
@@ -2,18 +2,25 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
 
 const Insights = () => {
+
+    const [openText, setOpenText] = useState<number | null>(null);
+    const [activeText, setActiveText] = useState<string | null>(null);
 
     const settings = {
         dots: false,
         arrows: false,
         infinite: true,
-        speed: 400,
-        slidesToShow: 1,
+        speed: 1500,
+        slidesToShow: 2,
         slidesToScroll: 1,
         centerMode: true,
-        centerPadding: "16px",
+        centerPadding: "0px",
+        autoplay: true,
+        autoplaySpeed: 2500,
+        pauseOnHover: true,
         responsive: [
             {
                 breakpoint: 1024,
@@ -21,6 +28,7 @@ const Insights = () => {
                     slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
+                    centerPadding: "16px",
                 }
             },
             {
@@ -30,6 +38,8 @@ const Insights = () => {
                     slidesToScroll: 1,
                     initialSlide: 1,
                     infinite: true,
+                    centerPadding: "16px",
+
                 }
             },
         ]
@@ -52,53 +62,14 @@ const Insights = () => {
                         </span>
                     </button>
                 </div>
-                {/* Web Card design */}
-                <div className="w-full max-w-[770px] hidden lg:flex justify-center items-center gap-6 md:gap-[30px]">
-                    <div className="w-full max-w-[370px] flex flex-col justify-center items-center rounded-xl bg-white">
-                        <div className="">
-                            <img src="assets/images/alt-EsCommerce-1.png" alt="" loading="lazy" className="rounded-tl-xl rounded-tr-xl" />
-                        </div>
-                        <div className="flex flex-col justify-center items-center gap-3.5 md:gap-4 p-4 md:p-5">
-                            <span className="text-black font-normal text-sm sm:text-[15px] leading-normal md:leading-[22px]">SEBI eases compliance for alternative funds, enabling flexible benchmarking. SEBI eases For information about the accessibility of our websites, applications, content and services, please visit our... Read More</span>
-                            <div className="flex justify-between items-center w-full">
-                                <div className="flex justify-start items-center gap-3 md:gap-4">
-                                    <img src="assets/icons/alt-user-icn.svg" alt="" className="w-auto h-[42px] md:h-auto" />
-                                    <div className="flex flex-col gap-0.5 justify-center items-start">
-                                        <span className="text-black font-semibold text-sm sm:text-base">EsCommerce</span>
-                                        <span className="text-black/60 font-normal text-xs sm:text-sm">21/06/2024</span>
-                                    </div>
-                                </div>
-                                <img src="assets/icons/alt-linkedin.svg" alt="" className="w-auto h-[22px] md:h-auto" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full max-w-[370px] flex flex-col justify-center items-center rounded-xl bg-white">
-                        <div className="">
-                            <img src="assets/images/alt-EsCommerce-2.png" alt="" loading="lazy" className="rounded-tl-xl rounded-tr-xl" />
-                        </div>
-                        <div className="flex flex-col justify-center items-center gap-3.5 md:gap-4 p-4 md:p-5">
-                            <span className="text-black font-normal text-sm sm:text-[15px] leading-normal md:leading-[22px]">If we are notified that any User Content infringes another’s intellectual property rights, we may remove such User Content pursuant to the DMCA. In accordance with the DMCA, we have a repeat infringer policy.</span>
-                            <div className="flex justify-between items-center w-full">
-                                <div className="flex justify-start items-center gap-3 md:gap-4">
-                                    <img src="assets/icons/alt-user-icn.svg" alt="" className="w-auto h-[42px] md:h-auto" />
-                                    <div className="flex flex-col gap-0.5 justify-center items-start">
-                                        <span className="text-black font-semibold text-sm sm:text-base">EsCommerce</span>
-                                        <span className="text-black/60 font-normal text-xs sm:text-sm">21/06/2024</span>
-                                    </div>
-                                </div>
-                                <img src="assets/icons/alt-linkedin.svg" alt="" className="w-auto h-[22px] md:h-auto" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 {/* mobile Card design */}
                 {/* RIGHT – CAROUSEL */}
-                <div className="lg:hidden block w-full">
+                <div className="w-full lg:max-w-[770px] max-w-full insights">
                     <Slider {...settings} className="insights-slider">
 
                         {/* CARD 1 */}
-                        <div className="px-2">
+                        <div className="px-2 lg:pl-[30px] lg:px-0">
                             <div className="flex flex-col rounded-xl bg-white h-full">
                                 <img
                                     src="assets/images/alt-EsCommerce-1.png"
@@ -107,10 +78,25 @@ const Insights = () => {
                                     className="rounded-t-xl md:h-auto h-[260px]"
                                 />
                                 <div className="flex flex-col gap-3.5 md:gap-4 p-4 md:p-5">
-                                    <span className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]">
-                                        SEBI eases compliance for alternative funds, enabling flexible benchmarking. SEBI eases For information about the accessibility of our websites, applications, content and services, please visit our... Read More
+                                    <span
+                                        className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]
+      line-clamp-6 lg:line-clamp-5"
+                                    >
+                                        SEBI eases compliance for alternative funds, enabling flexible benchmarking.
+                                        SEBI eases For information about the accessibility of our websites, applications,
+                                        content and services, please visit our... <a
+                                            href="#"
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                setActiveText(
+                                                    "SEBI eases compliance for alternative funds, enabling flexible benchmarking. SEBI eases For information about the accessibility of our websites, applications, content and services, please visit our full detailed article content here..."
+                                                );
+                                            }}
+                                            className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]"
+                                        >
+                                            Read More
+                                        </a>
                                     </span>
-
                                     <div className="flex justify-between items-center">
                                         <div className="flex gap-3 items-center">
                                             <img src="assets/icons/alt-user-icn.svg" alt="" className="w-auto h-[42px] md:h-auto" />
@@ -126,7 +112,7 @@ const Insights = () => {
                         </div>
 
                         {/* CARD 2 */}
-                        <div className="px-2">
+                        <div className="px-2 lg:pl-[30px] lg:px-0">
                             <div className="flex flex-col rounded-xl bg-white h-full">
                                 <img
                                     src="assets/images/alt-EsCommerce-2.png"
@@ -154,7 +140,7 @@ const Insights = () => {
                         </div>
 
                         {/* CARD 1 */}
-                        <div className="px-2">
+                        <div className="px-2 lg:pl-[30px] lg:px-0">
                             <div className="flex flex-col rounded-xl bg-white h-full">
                                 <img
                                     src="assets/images/alt-EsCommerce-1.png"
@@ -163,9 +149,25 @@ const Insights = () => {
                                     className="rounded-t-xl md:h-auto h-[260px]"
                                 />
                                 <div className="flex flex-col gap-3.5 md:gap-4 p-4 md:p-5">
-                                    <span className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]">
-                                        SEBI eases compliance for alternative funds, enabling flexible benchmarking. SEBI eases For information about the accessibility of our websites, applications, content and services, please visit our... Read More
+                                    <span
+                                        className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px] line-clamp-6 lg:line-clamp-5"
+                                    >
+                                        SEBI eases compliance for alternative funds, enabling flexible benchmarking.
+                                        SEBI eases For information about the accessibility of our websites, applications,
+                                        content and services, please visit our... <a
+                                            href="#"
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                setActiveText(
+                                                    "SEBI eases compliance for alternative funds, enabling flexible benchmarking. SEBI eases For information about the accessibility of our websites, applications, content and services, please visit our full detailed article content here..."
+                                                );
+                                            }}
+                                            className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]"
+                                        >
+                                            Read More
+                                        </a>
                                     </span>
+
 
                                     <div className="flex justify-between items-center">
                                         <div className="flex gap-3 items-center">
@@ -182,7 +184,7 @@ const Insights = () => {
                         </div>
 
                         {/* CARD 2 */}
-                        <div className="px-2">
+                        <div className="px-2 lg:pl-[30px] lg:px-0">
                             <div className="flex flex-col rounded-xl bg-white h-full">
                                 <img
                                     src="assets/images/alt-EsCommerce-2.png"
@@ -194,7 +196,6 @@ const Insights = () => {
                                     <span className="text-black text-sm sm:text-[15px] leading-[21px] md:leading-[22px]">
                                         If we are notified that any User Content infringes another’s intellectual property rights, we may remove such User Content pursuant to the DMCA. In accordance with the DMCA, we have a repeat infringer policy.
                                     </span>
-
                                     <div className="flex justify-between items-center">
                                         <div className="flex gap-3 items-center">
                                             <img src="assets/icons/alt-user-icn.svg" alt="" className="w-auto h-[42px] md:h-auto" />
@@ -209,6 +210,29 @@ const Insights = () => {
                             </div>
                         </div>
                     </Slider>
+                    {activeText && (
+                        <div className="fixed inset-0 z-[999] flex items-center justify-center bg-black/60 px-4">
+                            <div className="bg-white max-w-[600px] w-full rounded-xl p-6 relative animate-fadeIn">
+
+                                {/* Close Button */}
+                                <button
+                                    onClick={() => setActiveText(null)}
+                                    className="absolute right-4 top-4 text-black text-xl font-bold cursor-pointer"
+                                >
+                                    ✕
+                                </button>
+
+                                {/* Content */}
+                                <div className="max-h-[70vh] overflow-y-auto pr-2">
+                                    <p className="text-black text-sm sm:text-[15px] leading-[22px]">
+                                        {activeText}
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+
                 </div>
             </div>
         </div>
